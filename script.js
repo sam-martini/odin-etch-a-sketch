5f87f00cef3003cd014bcdd08a0b3dbdaf397553
sam-martini <113351225+sam-martini@users.noreply.github.com>
2023-02-08 14:55:58 +1100

    feat: add drawing functionality; use css variable

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 const gridContainer = document.querySelector('.grid-container');
 
-
 // create a function that takes in a number and generates a grid with that number of even squares
 // per side. 
 function createGrid(size) {
@@ -20,3 +19,33 @@ function createGrid(size) {
 }
 
 createGrid(8);
+
+
+
+
+
+// get each square from the grid using queryselectorAll 
+// addeventlistener to all and look for a click
+// call a function that changes the background color of the 
+// target square by using a css variable
+
+//  the color choice is stored in a variable that will change
+//  depending on user input
+
+let colorChoice = 'black';
+
+function changeBackground(e) {
+    console.log(e.target);
+    e.target.style.setProperty('--color-choice', colorChoice);
+}
+
+function getSquares() {
+    const squares = gridContainer.querySelectorAll('.square');
+    console.log(squares);
+
+    squares.forEach(square => {
+        square.addEventListener('click', changeBackground);
+    });
+}
+
+getSquares();
